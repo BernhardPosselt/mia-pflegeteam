37e3ca8f6dc32db9c9f8d57e4b9d84ba36d1be26
Bernhard Posselt <bernhard.posselt@gmx.at>
2012-07-26 02:18:35 +0200

    navi css

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 
 // root url, change this to /mia for instance to prefix every url with /mia
-$siteRoot;
+$siteRoot = "/";
+
 $tpls = array(
     "index" => "tpl/index.php"
 );
@@ -18,7 +19,7 @@ if (isset($_GET["id"])) {
 <html>
 
 <head>
-    <title>Mia Pflegeteam</title>
+    <title>Pia Pflegeteam</title>
     <link rel="icon" href="<?php echo $siteRoot ?>style/img/favicon.ico" type="image/x-icon"/>
     <link rel="shortcut icon" href="<?php echo $siteRoot ?>style/img/favicon.ico" type="image/x-icon"/> 
     <link rel="stylesheet" type="text/css" href="<?php echo $siteRoot ?>style/lib/normalize.css"/>
@@ -28,14 +29,24 @@ if (isset($_GET["id"])) {
 
 <body>
     <div id="site">
+        
         <header></header>
-        <nav></nav>
+        
+        <nav>
+            <ul>
+                <li><a href="#">Menu1</a></li>
+                <li class="active"><a href="#">Menu2</a></li>
+            </ul>
+        </nav>
+        
         <div id="content">
-        <?php
-        include_once($tpl);
-        ?>
+            <?php
+            include_once($tpl);
+            ?>
         </div>
+        
         <footer></footer>
+
     </div>
 </body>
     
