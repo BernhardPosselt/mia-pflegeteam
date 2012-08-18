fe44b84a0321a628ebf6154888cd8381479aa989
Bernhard Posselt <nukeawhale@gmail.com>
2012-08-18 16:12:51 +0200

    added contact page

--- tpl/contact.php
+++ tpl/contact.php
@@ -1,3 +1,26 @@
-<h1>hi</h1>
+<div class="text_block">
+    <h1>Kontaktinformationen:</h1>
+    <table>
+        <tr>
+            <th>Name:</th>
+            <td>Anja Becker</td>
+        </tr>
+        <tr>
+            <th>E-Mail:</th>
+            <td><a href="mailto:s.anja@pflegeteam-pia.de">s.anja@pflegeteam-pia.de</a></td>
+        </tr>
+        <tr>
+            <th>Anschrift:</th>
+            <td>Tessiner Straße 10, 18507 Rostock</td>
+        </tr>
+        <tr>
+            <th>Telefon Nummer:</th>
+            <td>0381 / 46 22 957 3</td>
+        </tr>
+    </table>
+</div>
 
-<p>hi</p>
+<div class="text_block">
+    <h1>Anfahrt:</h1>
+    <img src="<?php echo $siteRoot ?>img/anfahrt.png" alt="anfahrt">
+</div>
