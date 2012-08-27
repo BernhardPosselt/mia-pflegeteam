b69c5127802f4b2b82dd08882e289921cf67d2f1
Bernhard Posselt <nukeawhale@gmail.com>
2012-08-27 16:47:36 +0200

    added application site and sitenotice

---
+++ tpl/application.php
@@ -0,0 +1,49 @@
+<h1>Wir suchen Sie:</h1>
+
+<p>Das Pflegeteam PIA ist ein ambulanter Pflegedienst, dessen Ziel es ist eine hochwertige häusliche Versorgung zu gewährleisten.
+Unser junges Team ermöglicht den Kunden ein selbstbestimmtes Leben in vertrauter Umgebung.</p>
+
+<h2>Zur Erweiterung unseres Kollegiums suchen wir ab sofort:</h2>
+<ul>
+    <li>Krankenpfleger (m/w)</li>
+    <li>Altenpfleger (m/w)</li>
+    <li>Altenpflegehelfer (m/w)</li>
+</ul>
+
+<h2>Anforderungen:</h2>
+<ul>
+    <li>Teamfähigkeit</li>
+    <li>Körperliche Belastbarkeit</li>
+    <li>Einsatzbereitschaft</li>
+    <li>Sehr gute Umgangsformen</li>
+    <li>Führerschein Klasse B</li>
+</ul>
+
+<h2>Aufgaben:</h2>
+<ul>
+    <li>Ambulante Kranken- und Behandlungspflege</li>
+    <li>Führung und Aktualisierung der Pflegedokumentation</li>
+    <li>Umsetzung und Überwachung des Pflegeprozesses</li>
+</ul>
+
+<p><b>Bitte senden Sie uns Ihre vollständigen Bewerbungsunterlagen (Lebenslauf,
+Zeugniskopien, Eintrittstermin) vorzugsweise in elektronischer Form an:</b></p>
+
+<table>
+    <tr>
+        <th>Name:</th>
+        <td>Pflegeteam PIA</td>
+    </tr>
+    <tr>
+        <th>E-Mail:</th>
+        <td><a href="mailto:s.anja@pflegeteam-pia.de">s.anja@pflegeteam-pia.de</a></td>
+    </tr>
+    <tr>
+        <th>Anschrift:</th>
+        <td>Tessiner Straße 10, 18057 Rostock</td>
+    </tr>
+    <tr>
+        <th>Telefonnummer:</th>
+        <td>0172 / 784 16 41</td>
+    </tr>
+</table>
---
+++ tpl/sitenotice.php
@@ -0,0 +1,24 @@
+<h1>Impressum</h1>
+
+<table>
+    <tr>
+        <th>Name:</th>
+        <td>Anja Becker</td>
+    </tr>
+    <tr>
+        <th>E-Mail:</th>
+        <td><a href="mailto:s.anja@pflegeteam-pia.de">s.anja@pflegeteam-pia.de</a></td>
+    </tr>
+    <tr>
+        <th>Anschrift:</th>
+        <td>Tessiner Straße 10, 18057 Rostock</td>
+    </tr>
+    <tr>
+        <th>Steuernummer:</th>
+        <td>079/205/03432</td>
+    </tr>
+    <tr>
+        <th>Telefonnummer:</th>
+        <td>0172 / 784 16 41</td>
+    </tr>
+</table>
